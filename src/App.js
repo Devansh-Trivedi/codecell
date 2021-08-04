61d5491095285fb023e7a4b5d1cc376ffbeba59d
Devansh-Trivedi <tridevansh.160601@gmail.com>
2021-08-04 09:42:53 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
+
+import {BrowserRouter,Route,Switch,useHistory,Redirect} from 'react-router-dom'
+
+import Index from "views/Index.js";
+import LandingPage from "views/examples/LandingPage.js";
+import RegisterPage from "views/examples/RegisterPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+const Routing = ()=>{
+  
+  return(
+    
+    <BrowserRouter>
+    <Switch>
+      <Route path="/components" render={(props) => <Index {...props} />} />
+      <Route
+        path="/landing-page"
+        render={(props) => <LandingPage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={(props) => <RegisterPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={(props) => <ProfilePage {...props} />}
+      />
+      <Redirect from="/" to="/landing-page" />
+    </Switch>
+  </BrowserRouter>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <NavBar /> */}
+      <Routing />
+      
+    </BrowserRouter>
   );
 }
 
